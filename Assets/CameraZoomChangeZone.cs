519280e7914b1146eff7da47e25fa89bb9b4a7b4
zanthous <bcm023@gmail.com>
2021-04-27 08:27:20 -0400

    added untracked files

---
+++ Assets/CameraZoomChangeZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class CameraZoomChangeZone : MonoBehaviour
+{
+    //What should the camera's zoom level change to after hitting this collider?
+    [SerializeField] private CameraZoomLevel zoomLevel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            CameraFollow.ChangeCameraZoom.Invoke(zoomLevel);
+        }
+    }
+}
